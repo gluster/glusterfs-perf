90cdc21e1964d1f3ecacf2a64d75542f8d7f1bf3
Amar Tumballi <amarts@gmail.com>
2019-02-28 10:12:25 +0530

    plot: add option to capture difference between yesterday's run and current run (#19)

--- plot/compare-and-plot.py
+++ plot/compare-and-plot.py
@@ -11,12 +11,28 @@ import csv
 import json
 from pprint import pprint
 import sys, getopt
-import datetime
+from datetime import datetime, timedelta
 import random
+
+def read_current_values(rdir, obj, k):
+    try:
+        with open("%s/%s" % (rdir, k)) as f:
+            data = json.load(f)
+        obj[k] = data['results']['files-per-sec']
+    except:
+        obj[k] = 1.0
 
 def plot_graph(ref, cur, plot_label, tags):
     # set width of bar
     barWidth = 1.0 / (2 + len(tags))
+
+    #get nightly dir
+    prev_val = {}
+    yesterday = datetime.strftime(datetime.now() - timedelta(1), "%Y-%m-%d")
+    nightly = "/var/tmp/glusterperf/%s-nightly" % yesterday
+    # actually get the perf number from the results file.
+    for key in plot_label:
+        read_current_values(nightly, prev_val, key)
 
     # Set position of bar on X axis
     r1 = np.arange(len(cur))
@@ -24,7 +40,17 @@ def plot_graph(ref, cur, plot_label, tags):
     # Make the plot
     plt.bar(r1, [cur[k] for k in plot_label], color='#7f6d5f', width=barWidth, edgecolor='white', label="this")
 
-    prev_rtag = r1
+    # graph with yesterday night run.
+    rtag = [x + barWidth for x in r1]
+    r = lambda: random.randint(0,255)
+    plt.bar(rtag, [prev_val[k] for k in plot_label], color='#%02X%02X%02X' % (r(), r(), r()), width=barWidth, edgecolor='white', label="yesterday")
+    print("==== Comparision from Tag: yesterday nightly ====\n")
+    for key in plot_label:
+        cval = cur[key]
+        pval = prev_val[key]
+        print ("%-15s %13f -> %13f %11f%%" % (key, pval, cval, ((cval - pval) / pval) * 100))
+
+    prev_rtag = rtag
     for t in tags:
         rtag = [x + barWidth for x in prev_rtag]
         prev_rtag = rtag
@@ -71,18 +97,12 @@ def load_reference_values(ops, tags):
             for key in ops:
                 ref_value[tag][key] = float(row[key])
 
-def read_current_values(obj, k):
-    with open("%s/%s" % (result_dir, k)) as f:
-        data = json.load(f)
-    obj[k] = data['results']['files-per-sec']
-
-
 def dump_results(ops, ref, tags):
     cur_res = {}
 
     # actually get the perf number from the results file.
     for key in ops:
-        read_current_values(cur_res, key)
+        read_current_values(result_dir, cur_res, key)
 
     plot_graph(ref, cur_res, ops, tags)
 
@@ -117,9 +137,9 @@ def main(argv):
     ops = ['create', 'delete', 'append', 'overwrite', 'rename', 'mkdir', 'rmdir', 'symlink', 'setxattr', 'chmod', 'delete-renamed', 'read', 'readdir', 'stat', 'getxattr', 'ls-l' ]
     tags = []
 
-    d = datetime.datetime.now()
+    d = datetime.now()
     current_tag = d.strftime("%Y-%m-%d")
-
+    yesterday = datetime.strftime(d - timedelta(1), "%Y-%m-%d")
     parse_args(argv[0], argv[1:])
     load_reference_values(ops, tags)
 
